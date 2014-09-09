a1620a3cc3f12214243c321cc4588332c125a5b1
Mateo Olaya Bernal <molayab@Mateos-MacBook-Air.local>
2014-09-09 13:46:02 -0500

    Se agrega la parte de iOS

---
+++ NoteHub/NoteHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NoteHub
+//
+//  Created by Mateo Olaya Bernal on 9/9/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
