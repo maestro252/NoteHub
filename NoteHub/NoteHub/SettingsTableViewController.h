8449a519e782b99cdf3fb862f1a535e2ab112080
Mateo Olaya Bernal <molayab@Mateos-MacBook-Air.local>
2014-09-09 15:50:10 -0500

    se agrega la interfaz basica del home

---
+++ NoteHub/NoteHub/SettingsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewController.h
+//  NoteHub
+//
+//  Created by Mateo Olaya Bernal on 9/9/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+{
+    __weak IBOutlet UITableViewCell *offline;
+    
+}
+@end
