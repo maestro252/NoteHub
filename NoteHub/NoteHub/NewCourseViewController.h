4092086320b55bdd132a6be92ae8bd5f6fa4063f
Jonathan Eidelman <eidelmanjonathan@hotmail.com>
2014-10-08 15:48:18 -0500

    Se agrega la vista inicial para crear un curso, falta implementarlo

---
+++ NoteHub/NoteHub/NewCourseViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewCourseViewController.h
+//  NoteHub
+//
+//  Created by Jonathan Eidelman on 8/10/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewCourseViewController : UIViewController
+
+@end
---
+++ NoteHub/NoteHub/NewCourseViewController.m
@@ -0,0 +1,37 @@
+//
+//  NewCourseViewController.m
+//  NoteHub
+//
+//  Created by Jonathan Eidelman on 8/10/14.
+//  Copyright (c) 2014 Mateo Olaya Bernal. All rights reserved.
+//
+
+#import "NewCourseViewController.h"
+
+@interface NewCourseViewController ()
+
+@end
+
+@implementation NewCourseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
